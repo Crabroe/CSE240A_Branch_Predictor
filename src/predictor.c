d4173e8c264acb7b8f4f0fdd828297e550cbde0f
Crabroe <843196003@qq.com>
2024-03-18 13:34:28 -0700

    'update'

--- src/predictor.c
+++ src/predictor.c
@@ -41,14 +41,14 @@ uint32_t ghistory;          // Global history register
 uint8_t *gshareBHT;         // Branch History Table
 //Tournament
 uint32_t *localHistoryTable; // Local History Table
-uint8_t *localBHT;           // Local Branch History Table
-uint8_t *globalBHT;          // Global Branch History Table
-uint8_t *choiceBHT;          // Choice Predictor Table
-//Custom
+uint32_t *localBHT;           // Local Branch History Table
+uint32_t *globalBHT;          // Global Branch History Table
+uint32_t *choiceBHT;          // Choice Predictor Table
 
 //------------------------------------//
 //        Predictor Functions         //
 //------------------------------------//
+
 
 // Initialize the predictor
 //
@@ -64,9 +64,9 @@ void init_predictor() {
     
     case TOURNAMENT:
       localHistoryTable = (uint32_t *)malloc((1 << pcIndexBits) * sizeof(uint32_t));
-      localBHT = (uint8_t *)malloc((1 << lhistoryBits) * sizeof(uint8_t));
-      globalBHT = (uint8_t *)malloc((1 << ghistoryBits) * sizeof(uint8_t));
-      choiceBHT = (uint8_t *)malloc((1 << ghistoryBits) * sizeof(uint8_t));
+      localBHT = (uint32_t *)malloc((1 << lhistoryBits) * sizeof(uint32_t));
+      globalBHT = (uint32_t *)malloc((1 << ghistoryBits) * sizeof(uint32_t));
+      choiceBHT = (uint32_t *)malloc((1 << ghistoryBits) * sizeof(uint32_t));
       for (int i = 0; i < (1 << pcIndexBits); i++) {
         localHistoryTable[i] = 0;
       }
@@ -75,16 +75,16 @@ void init_predictor() {
       }
       for (int i = 0; i < (1 << ghistoryBits); i++) {
         globalBHT[i] = WN;
-        choiceBHT[i] = WN;
+        choiceBHT[i] = WT;
       }
       ghistory = 0;
       break;
     
-    case CUSTOM:
+    case CUSTOM: 
       localHistoryTable = (uint32_t *)malloc((1 << pcIndexBits) * sizeof(uint32_t));
-      localBHT = (uint8_t *)malloc((1 << lhistoryBits) * sizeof(uint8_t));
-      globalBHT = (uint8_t *)malloc((1 << ghistoryBits) * sizeof(uint8_t));
-      choiceBHT = (uint8_t *)malloc((1 << ghistoryBits) * sizeof(uint8_t));
+      localBHT = (uint32_t *)malloc((1 << lhistoryBits) * sizeof(uint32_t));
+      globalBHT = (uint32_t *)malloc((1 << ghistoryBits) * sizeof(uint32_t));
+      choiceBHT = (uint32_t *)malloc((1 << ghistoryBits) * sizeof(uint32_t));
       for (int i = 0; i < (1 << pcIndexBits); i++) {
         localHistoryTable[i] = 0;
       }
@@ -93,21 +93,22 @@ void init_predictor() {
       }
       for (int i = 0; i < (1 << ghistoryBits); i++) {
         globalBHT[i] = WN;
-        choiceBHT[i] = WN;
+        choiceBHT[i] = WT;
       }
       ghistory = 0;
       break;
+    
     default:
       break;
   }
 }
-
 
 // Make a prediction for conditional branch instruction at PC 'pc'
 // Returning TAKEN indicates a prediction of taken; returning NOTTAKEN
 // indicates a prediction of not taken
 //
 uint8_t make_prediction(uint32_t pc) {
+  // Make a prediction based on the bpType
   switch (bpType) {
     case STATIC:
       return TAKEN;
@@ -126,7 +127,7 @@ uint8_t make_prediction(uint32_t pc) {
 
       if(localBHT[localBHTID] >= WT){localPrediction=TAKEN;}
       else{localPrediction=NOTTAKEN;}
-
+      
       uint32_t globalHistory = ghistory & ((1 << ghistoryBits) - 1);
       uint8_t globalPrediction; 
 
@@ -141,7 +142,29 @@ uint8_t make_prediction(uint32_t pc) {
       } else {
         return localPrediction;
       }
+      // uint32_t pred;
+      // uint32_t localID;
+      // uint32_t localHistory;
+      // uint32_t localBHTID;
+      // uint32_t globalHistory = ghistory & ((1 << ghistoryBits) - 1);
+      // if(choiceBHT[globalHistory]<WT)
+      // {
+      //   localID = pc & ((1 << pcIndexBits) - 1);
+      //   localHistory = localHistoryTable[localID];
+      //   localBHTID = localHistory & ((1 << lhistoryBits) - 1);
+      //   pred = localBHT[localBHTID];
+      // }
+      // else
+      // {
+      //   pred = globalBHT[globalHistory];
+      // }
+      // if(pred>WN)
+      //   return TAKEN;
+      // else
+      //   return NOTTAKEN;
+
     }
+
     case CUSTOM: {
       uint32_t localID = pc & ((1 << pcIndexBits) - 1);
       uint32_t localHistory = localHistoryTable[localID];
@@ -151,8 +174,10 @@ uint8_t make_prediction(uint32_t pc) {
 
       if(localBHT[localBHTID] >= WT){localPrediction=TAKEN;}
       else{localPrediction=NOTTAKEN;}
-
-      uint32_t globalHistory = (pc ^ ghistory) & ((1 << ghistoryBits) - 1);
+      
+      uint32_t xor=pc ^ ghistory;
+      uint32_t globalHistory = xor&((1 << ghistoryBits) - 1);
+      // uint32_t globalHistory = ghistory & ((1 << ghistoryBits) - 1);
       uint8_t globalPrediction; 
 
       if(globalBHT[globalHistory] >= WT){globalPrediction=TAKEN;}
@@ -166,7 +191,7 @@ uint8_t make_prediction(uint32_t pc) {
       } else {
         return localPrediction;
       }
-    }
+    } 
     default:
       return NOTTAKEN;
   }
@@ -194,102 +219,83 @@ void train_predictor(uint32_t pc, uint8_t outcome) {
     }
     case TOURNAMENT: {
       uint32_t localID = pc & ((1 << pcIndexBits) - 1);
+      uint32_t globalHistory = ghistory & ((1 << ghistoryBits) - 1);
       uint32_t localHistory = localHistoryTable[localID];
       uint32_t localBHTID = localHistory & ((1 << lhistoryBits) - 1);
+      uint32_t lprediction = localBHT[localBHTID];
+      if(lprediction>WT)
+        lprediction = TAKEN;
+      else
+        lprediction = NOTTAKEN;
+      uint32_t gprediction = globalBHT[globalHistory];
+      if(gprediction>WT)
+        gprediction = TAKEN;
+      else
+        gprediction = NOTTAKEN;
 
-      if (outcome == TAKEN) {
-        if(localBHT[localBHTID] == ST){localBHT[localBHTID]=ST;}
-        else{localBHT[localBHTID]=localBHT[localBHTID] + 1;}
-      } 
-      else {
-        if(localBHT[localBHTID] == SN){localBHT[localBHTID] = SN;}
-        else{localBHT[localBHTID] = localBHT[localBHTID] - 1;}
+      if(gprediction==outcome && lprediction!=outcome && choiceBHT[globalHistory]!=ST)
+        choiceBHT[globalHistory]= choiceBHT[globalHistory] + 1;
+      else if(gprediction!=outcome && lprediction==outcome && choiceBHT[globalHistory]!=SN)
+        choiceBHT[globalHistory]= choiceBHT[globalHistory] - 1;
+      if(outcome==TAKEN)
+      {
+        if(globalBHT[globalHistory]!=ST)
+          globalBHT[globalHistory] = globalBHT[globalHistory] + 1;
+        if(localBHT[localBHTID]!=ST)
+          localBHT[localBHTID]=localBHT[localBHTID] + 1;;
       }
-
-      uint32_t globalHistory = ghistory & ((1 << ghistoryBits) - 1);
-
-      if (outcome == TAKEN) {
-        if(globalBHT[globalHistory] == ST){globalBHT[globalHistory] = ST;}
-        else{globalBHT[globalHistory] = globalBHT[globalHistory] + 1;}
-      } else {
-        if(globalBHT[globalHistory] == SN){globalBHT[globalHistory] = SN;}
-        else{globalBHT[globalHistory] = globalBHT[globalHistory] - 1;}
+      else
+      {
+        if(globalBHT[globalHistory]!=SN)
+          globalBHT[globalHistory] = globalBHT[globalHistory] - 1;
+        if(localBHT[localBHTID]!=SN)
+          localBHT[localBHTID] = localBHT[localBHTID] - 1;;
       }
-
-      uint8_t localPrediction;
-      uint8_t globalPrediction;
-
-      if(localBHT[localBHTID] >= WT){localPrediction=TAKEN;}
-      else{localPrediction=NOTTAKEN;}
-
-      if(globalBHT[globalHistory] >= WT){globalPrediction=TAKEN;}
-      else{globalPrediction=NOTTAKEN;}
-
-      uint32_t choiceID = globalHistory;
-
-      if (localPrediction != globalPrediction) {
-        if (outcome == localPrediction) {
-          if(choiceBHT[choiceID] == SN){choiceBHT[choiceID] = SN;}
-          else{choiceBHT[choiceID] = choiceBHT[choiceID] -1;}
-        } else if (outcome == globalPrediction) {
-          if(choiceBHT[choiceID] == ST){choiceBHT[choiceID] = ST;}
-          else{choiceBHT[choiceID] = choiceBHT[choiceID] = +1;}
-        }
-      }
-
       localHistoryTable[localID] = ((localHistory << 1) | outcome) & ((1 << lhistoryBits) - 1);
       ghistory = ((ghistory << 1) | outcome) & ((1 << ghistoryBits) - 1);
-      break;
+      break;  
     }
     case CUSTOM: {
 
       uint32_t localID = pc & ((1 << pcIndexBits) - 1);
+      uint32_t xor=pc ^ ghistory;
+      uint32_t globalHistory = xor&((1 << ghistoryBits) - 1);
+      // uint32_t globalHistory = ghistory & ((1 << ghistoryBits) - 1);
       uint32_t localHistory = localHistoryTable[localID];
       uint32_t localBHTID = localHistory & ((1 << lhistoryBits) - 1);
+      uint32_t lprediction = localBHT[localBHTID];
+      if(lprediction>WT)
+        lprediction = TAKEN;
+      else
+        lprediction = NOTTAKEN;
+      uint32_t gprediction = globalBHT[globalHistory];
+      if(gprediction>WT)
+        gprediction = TAKEN;
+      else
+        gprediction = NOTTAKEN;
 
-      if (outcome == TAKEN) {
-        if(localBHT[localBHTID] == ST){localBHT[localBHTID]=ST;}
-        else{localBHT[localBHTID]=localBHT[localBHTID] + 1;}
-      } 
-      else {
-        if(localBHT[localBHTID] == SN){localBHT[localBHTID] = SN;}
-        else{localBHT[localBHTID] = localBHT[localBHTID] - 1;}
+      if(gprediction==outcome && lprediction!=outcome && choiceBHT[globalHistory]!=ST)
+        choiceBHT[globalHistory]= choiceBHT[globalHistory] + 1;
+      else if(gprediction!=outcome && lprediction==outcome && choiceBHT[globalHistory]!=SN)
+        choiceBHT[globalHistory]= choiceBHT[globalHistory] - 1;
+      if(outcome==TAKEN)
+      {
+        if(globalBHT[globalHistory]!=ST)
+          globalBHT[globalHistory] = globalBHT[globalHistory] + 1;
+        if(localBHT[localBHTID]!=ST)
+          localBHT[localBHTID]=localBHT[localBHTID] + 1;;
       }
-
-      uint32_t globalHistory = (pc ^ ghistory) & ((1 << ghistoryBits) - 1);
-
-      if (outcome == TAKEN) {
-        if(globalBHT[globalHistory] == ST){globalBHT[globalHistory] = ST;}
-        else{globalBHT[globalHistory] = globalBHT[globalHistory] + 1;}
-      } else {
-        if(globalBHT[globalHistory] == SN){globalBHT[globalHistory] = SN;}
-        else{globalBHT[globalHistory] = globalBHT[globalHistory] - 1;}
+      else
+      {
+        if(globalBHT[globalHistory]!=SN)
+          globalBHT[globalHistory] = globalBHT[globalHistory] - 1;
+        if(localBHT[localBHTID]!=SN)
+          localBHT[localBHTID] = localBHT[localBHTID] - 1;;
       }
-
-      uint8_t localPrediction;
-      uint8_t globalPrediction;
-
-      if(localBHT[localBHTID] >= WT){localPrediction=TAKEN;}
-      else{localPrediction=NOTTAKEN;}
-
-      if(globalBHT[globalHistory] >= WT){globalPrediction=TAKEN;}
-      else{globalPrediction=NOTTAKEN;}
-
-      uint32_t choiceID = globalHistory;
-
-      if (localPrediction != globalPrediction) {
-        if (outcome == localPrediction) {
-          if(choiceBHT[choiceID] == SN){choiceBHT[choiceID] = SN;}
-          else{choiceBHT[choiceID] = choiceBHT[choiceID] -1;}
-        } else if (outcome == globalPrediction) {
-          if(choiceBHT[choiceID] == ST){choiceBHT[choiceID] = ST;}
-          else{choiceBHT[choiceID] = choiceBHT[choiceID] = +1;}
-        }
-      }
-
       localHistoryTable[localID] = ((localHistory << 1) | outcome) & ((1 << lhistoryBits) - 1);
       ghistory = ((ghistory << 1) | outcome) & ((1 << ghistoryBits) - 1);
-      break;
+      break;  
+
     }
   }
 }
